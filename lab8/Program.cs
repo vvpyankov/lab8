50396de2d4971a046f78a6389eb87f0d63739b8f
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-12 20:37:20 +0300

    Добавьте файлы проекта.

---
+++ lab8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach(DriveInfo d in drives)
+            {
+                Console.WriteLine("Название: {0}", d.Name);
+                if (d.IsReady)
+                {
+                    Console.WriteLine("Объем диска: {0}", d.TotalSize);
+                    Console.WriteLine("Свободно: {0}", d.AvailableFreeSpace);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
